0ea826edf297a3d470e8be879aa502306ef10b21
Ahmad Hamdi <6674568+theHamdiz@users.noreply.github.com>
2023-02-23 17:26:37 +0300

    Tidied up the code.
    Just added some comments and tidied up the code a bit.

--- Chat GPT.ts
+++ Chat GPT.ts
@@ -1,4 +1,7 @@
 async function main(workbook: ExcelScript.Workbook) {
+    
+    // Top level constants & variables.
+    
     const apiKey = workbook.getWorksheet("API").getRange("B1").getValue();
     const endPoint: string = "https://api.openai.com/v1/completions";
     const sheet = workbook.getWorksheet("Data");
@@ -12,7 +15,7 @@ async function main(workbook: ExcelScript.Workbook) {
     const productNames = sheet.getRange(productNamesRange).getValues();
     const model: string = "text-davinci-002";
 
-    sheet.getRange(keywordsRange).setValue(" ");
+    sheet.getRange(keywordsRange).setValue("");
 
     for (let i = 0; i < productsCount; i++){
         let productName = productNames[i].toString().replace("|", "");
@@ -34,8 +37,9 @@ async function main(workbook: ExcelScript.Workbook) {
         headers.append("Content-Type", "application/json");
         headers.append("Authorization", `Bearer ${apiKey}`);
 
-        // Set the HTTP Request Body
-        const body: (string| boolean | number) = JSON.stringify({
+        // Set the HTTP Request Body.
+        
+        const body: (string | boolean | number) = JSON.stringify({
             model: model,
             prompt: prompt,
             max_tokens: 2048,
@@ -43,6 +47,8 @@ async function main(workbook: ExcelScript.Workbook) {
             temperature: 1,
         });
 
+        // Get the response from ChatGPT.
+        
         const response: Response = await fetch(
             endPoint, {
                 method: "POST",
@@ -51,26 +57,37 @@ async function main(workbook: ExcelScript.Workbook) {
             }
         );
 
-
         const json: { choices: {text: (string | boolean | number)}[]} = await response.json();
-        const text: (string| boolean| number) = json.choices[0].text.toString();
+        const text: (string | boolean | number) = json.choices[0].text.toString();
 
+        // Get the cell in which we intend to store our data.
+        
         const output = sheet.getRange(`C${i+1}`);
-
+        
+        // Clean the data & store it.
+        
         output.setValue(cleanData(text));
     }
 }
 
 function cleanData(data: string){
-    const numberedString = data;
-    const listItems = numberedString.split("\n");
+    // Get each item of the response by using the new line as a separator.
+    const listItems = data.split("\n");
+    
+    // Remove potentially unwanted keywords & special characters.
     const regex = /\b(keyword(s)?|digital marketing|seo|\\|\/|\||\(|\)|\$|%|\^|&|\*|@|!|"|'|`)\b/gi;
     const legalKeywords = listItems.map((item) =>{
         return item.replace(regex, "");
     });
+    
+    // Get a unique list of responses.
     const uniqueStringList = Array.from(new Set(legalKeywords));
+    
+    // Remove leading numbers from each response.
     const listItemsWithoutNumbers = uniqueStringList.map((item) =>{
         return item.replace(/^\d+\.\s+/, "");
     });
+    
+    // Return a comma-separated keyword string.
     return listItemsWithoutNumbers.join(",");
 }
